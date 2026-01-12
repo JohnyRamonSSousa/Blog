aecfa062e867c57898f102eecf47ebcfc9e0038e
JohnyRamonSSousa <erikaandradesantosousa@gmail.com>
2026-01-12 20:29:29 -0300

    atualizando o site

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog One</title>
     <!-- ESTILO DO PROJETO -->
-    <link rel="stylesheet" href="<?=  $BASE_URL ?>/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <!-- GOOGLE FONT -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
